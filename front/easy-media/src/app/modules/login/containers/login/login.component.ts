ce6141174708bb61e495bb7a97f170cf1c40d3f3
Andres Munive <fmunive@getontop.com>
2023-09-07 12:35:35 -0500

    Login view implemented

---
+++ front/easy-media/src/app/modules/login/containers/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initLoginForm();
+  }
+
+  initLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+
+      // Aquí puedes implementar la lógica de autenticación
+    }
+  }
+}
